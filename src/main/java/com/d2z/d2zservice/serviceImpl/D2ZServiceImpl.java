932f26b48369508b76e1cef85f32fea129c5f2c5
Aparna <aparnakuthalanathan@gmail.com>
2018-09-10 21:11:58 +0530

    Label generation changes

--- src/main/java/com/d2z/d2zservice/serviceImpl/D2ZServiceImpl.java
+++ src/main/java/com/d2z/d2zservice/serviceImpl/D2ZServiceImpl.java
@@ -1,11 +1,15 @@
 package com.d2z.d2zservice.serviceImpl;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.d2z.d2zservice.dao.ID2ZDao;
 import com.d2z.d2zservice.entity.SenderdataMaster;
 import com.d2z.d2zservice.model.DropDownModel;
@@ -14,7 +18,14 @@ import com.d2z.d2zservice.model.SenderData;
 import com.d2z.d2zservice.model.UserMessage;
 import com.d2z.d2zservice.service.ID2ZService;
 
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.util.JRSaver;
 
 @Service
 public class D2ZServiceImpl implements ID2ZService{
@@ -55,7 +66,25 @@ public class D2ZServiceImpl implements ID2ZService{
 		         new JRBeanCollectionDataSource(senderData);
 		 Map<String,Object> parameters = new HashMap<>();
 		 //byte[] labelAsBytes = jasperService.generatePDFLabel(parameters, beanColDataSource);
-		return null;
+		 
+		 byte[] bytes = null;
+		    JasperReport jasperReport = null;
+		    try (ByteArrayOutputStream byteArray = new ByteArrayOutputStream()) {
+		    	
+		  
+		        String jrxml = "src/main/resources/eparcelLabel.jrxml";
+		        jasperReport = JasperCompileManager.compileReport(jrxml);
+		        // Save compiled report. Compiled report is loaded next time
+		        JRSaver.saveObject(jasperReport, "label.jasper");
+		      
+		      JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, beanColDataSource);
+		      // return the PDF in bytes
+		      bytes = JasperExportManager.exportReportToPdf(jasperPrint);
+		    }
+		    catch (JRException | IOException e) {
+		      e.printStackTrace();
+		    }
+		    return bytes;
 	}
 	
 
