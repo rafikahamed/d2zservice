cab5e71a07178f604a6d1b4fe7b7517f89aadc57
Aparna Kuthalanathan <Aparna Kuthalanathan@aparnakuthalanathan@gmail.com>
2018-11-28 01:04:55 +0530

    Etower API integration

---
+++ src/main/java/com/d2z/d2zservice/security/HMACGenerator.java
@@ -0,0 +1,39 @@
+package com.d2z.d2zservice.security;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.stereotype.Service;
+@Service
+public class HMACGenerator {
+
+	private final static String HMAC_SHA1_ALGORITHM = "HmacSHA1";
+	public String calculateHMAC(String key,String url,String verb) {
+		String DATE_FORMAT = "EEE, dd MMM yyyy HH:mm:ss zzz";
+		SimpleDateFormat currentDateFormat = new SimpleDateFormat(DATE_FORMAT, Locale.US);
+		currentDateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+		String currentDate = currentDateFormat.format(new Date());
+		
+		String toSignIn = verb+'\n'+currentDate+'\n'+url;
+		System.out.println(toSignIn);
+		String result = "";
+		try {
+		SecretKeySpec signingKey = new SecretKeySpec(key.getBytes(),HMAC_SHA1_ALGORITHM);
+		Mac mac = Mac.getInstance(HMAC_SHA1_ALGORITHM);
+		mac.init(signingKey);
+		//byte[] rawHmac=mac.doFinal(toSignIn.getBytes());
+	     result = Base64.encodeBase64String(mac.doFinal(toSignIn.getBytes()));
+	     System.out.println(Base64.decodeBase64(result));
+		} catch (Exception e) {
+		e.printStackTrace();
+		}
+	return result;
+		}
+}
