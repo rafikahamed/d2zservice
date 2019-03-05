a123e429cb122aeda0de3821797899881ddff111
“rahameda” <rahameda@paypal.com  git config --global user.email rahameda@paypal.com>
2019-03-05 22:54:11 +0530

    Broker List API Changes

--- src/main/java/com/d2z/d2zservice/model/BrokerList.java
+++ src/main/java/com/d2z/d2zservice/model/BrokerList.java
@@ -33,5 +33,43 @@ public class BrokerList {
 	public void setInjectionType(Set<DropDownModel> injectionType) {
 		this.injectionType = injectionType;
 	}
-
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((brokerUserName == null) ? 0 : brokerUserName.hashCode());
+		result = prime * result + ((injectionType == null) ? 0 : injectionType.hashCode());
+		result = prime * result + ((serviceType == null) ? 0 : serviceType.hashCode());
+		result = prime * result + userId;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BrokerList other = (BrokerList) obj;
+		if (brokerUserName == null) {
+			if (other.brokerUserName != null)
+				return false;
+		} else if (!brokerUserName.equals(other.brokerUserName))
+			return false;
+		if (injectionType == null) {
+			if (other.injectionType != null)
+				return false;
+		} else if (!injectionType.equals(other.injectionType))
+			return false;
+		if (serviceType == null) {
+			if (other.serviceType != null)
+				return false;
+		} else if (!serviceType.equals(other.serviceType))
+			return false;
+		if (userId != other.userId)
+			return false;
+		return true;
+	}
+	
 }
