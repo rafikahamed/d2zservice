c12c06c9cd7821e76f7393513a969f82d079cf16
Aparna <Aparna@DESKTOP-4VQOOD6>
2019-03-12 00:53:59 +0530

    Etower Production URL

--- src/main/java/com/d2z/d2zservice/proxy/ETowerProxy.java
+++ src/main/java/com/d2z/d2zservice/proxy/ETowerProxy.java
@@ -30,7 +30,8 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 public class ETowerProxy {
 	
 	
-	String baseURL = "http://qa-cn.etowertech.com/";
+	//String baseURL = "http://qa-cn.etowertech.com/";
+	String baseURL = "http://au.etowertech.com/";
 	public TrackingEventResponse makeCallForTrackingEvents(List<String> trackingNumber) {
 
 
