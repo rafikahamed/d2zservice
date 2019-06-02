720eea703f33551e295f6ee95892b988fe657d93
Aparna <Aparna@DESKTOP-4VQOOD6>
2019-06-02 20:53:46 +0530

    D2Z changes

--- src/main/java/com/d2z/d2zservice/serviceImpl/D2ZServiceImpl.java
+++ src/main/java/com/d2z/d2zservice/serviceImpl/D2ZServiceImpl.java
@@ -879,7 +879,7 @@ public class D2ZServiceImpl implements ID2ZService {
 				barcode.insert(41, '|');
 				barcode.insert(49, '|');
 				trackingDetail.setBarcode_id(barcode.toString());
-				trackingDetail.setConsignment_id(data.getArticleId().substring(0, 20));
+				trackingDetail.setConsignment_id(data.getArticleId().substring(0, 12));
 				item.setTracking_details(trackingDetail);
 
 				items.add(item);
