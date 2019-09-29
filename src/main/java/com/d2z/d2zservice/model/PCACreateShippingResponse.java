132d3ee70226b8747ea2a29aa44825cf6162e3c2
Aparna <Aparna@DESKTOP-4VQOOD6>
2019-09-29 23:34:00 +0530

    PCA Response fix

--- src/main/java/com/d2z/d2zservice/model/PCACreateShippingResponse.java
+++ src/main/java/com/d2z/d2zservice/model/PCACreateShippingResponse.java
@@ -1,5 +1,8 @@
 package com.d2z.d2zservice.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PCACreateShippingResponse {
 	
 	private int status;
